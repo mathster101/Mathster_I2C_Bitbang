e45da1662bf0324aa52e999084ff0f3b8a69472e
Mathew P <mathewsimonpanicker@gmail.com>
2021-09-14 21:43:32 +0530

    first commit

---
+++ Mathster_I2C_Bitbang.cpp
@@ -0,0 +1,15 @@
+#include "Mathster_I2C_Bitbang.h"
+
+/*
+class Mathster_I2C_Bitbang {
+public:
+  int SCL_PIN, SDA_PIN;
+  float I2C_FREQ;
+  float I2C_DELAY;
+  void Mathster_I2C_Bitbang();
+  void i2c_start();
+  void i2c_end();
+  void i2c_data_byte_out();
+  void i2c_data_byte_in();
+};
+*/
---
+++ Mathster_I2C_Bitbang.h
@@ -0,0 +1,13 @@
+#include "Arduino.h"
+
+class Mathster_I2C_Bitbang {
+public:
+  int SCL_PIN, SDA_PIN;
+  float I2C_FREQ;
+  float I2C_DELAY;
+  void Mathster_I2C_Bitbang();
+  void i2c_start();
+  void i2c_end();
+  void i2c_data_byte_out();
+  void i2c_data_byte_in();
+};
